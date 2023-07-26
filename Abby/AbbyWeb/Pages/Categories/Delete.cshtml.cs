f826c00afe3e02542dabdec0e8a7d97871fd0acf
NahidNml <fuad.allpic@gmail.com>
2023-07-26 22:26:55 +0600

    5th

--- Abby/AbbyWeb/Pages/Categories/Delete.cshtml.cs
+++ Abby/AbbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -7,34 +7,33 @@ namespace AbbyWeb.Pages.Categories
 {
 
     [BindProperties]
-    public class CreateModel : PageModel
+    public class DeleteModel : PageModel
     {
 
         private readonly ApplicationDbContext _db;
         public Category Category { get; set; }
-        
-        public CreateModel(ApplicationDbContext db)
+
+        public DeleteModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public void OnGet()
+        public void OnGet(int id)
         {
+            Category = _db.Category.Find(id);
         }
 
         public async Task<IActionResult> OnPost()
         {
-            if (Category.Name==Category.DisplayOrder.ToString())
+
+            var categoryFromDb = _db.Category.Find(Category.Id);
+            if (categoryFromDb != null)
             {
-                ModelState.AddModelError(String.Empty,"The DisplayOrder can not exactly match the Name.");
-            }
-            if (ModelState.IsValid)
-            {
-                await _db.Category.AddAsync(Category);
+                _db.Category.Remove(categoryFromDb);
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
             }
             return Page();
-            
+
         }
     }
 }
--- Abby/AbbyWeb/Pages/Categories/Edit.cshtml.cs
+++ Abby/AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -7,18 +7,19 @@ namespace AbbyWeb.Pages.Categories
 {
 
     [BindProperties]
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
 
         private readonly ApplicationDbContext _db;
         public Category Category { get; set; }
         
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public void OnGet()
+        public void OnGet( int id)
         {
+            Category = _db.Category.Find(id);
         }
 
         public async Task<IActionResult> OnPost()
@@ -29,7 +30,7 @@ namespace AbbyWeb.Pages.Categories
             }
             if (ModelState.IsValid)
             {
-                await _db.Category.AddAsync(Category);
+                _db.Category.Update(Category);
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
             }
